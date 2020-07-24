4f886d15bfda26dcaf56db4f2931dcd66a01f558
cornshek <616615110@qq.com>
2020-07-24 14:48:12 +0800

    ‘commit for test

---
+++ panda-txManager/src/test/java/com/pd/pandatxmanager/PandaTxmanagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pd.pandatxmanager;
+
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableTransactionManagerServer
+class PandaTxManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
